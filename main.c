03ed11e5bc7daa01ac722bfd72d369be57a3fd89
zrk <zrk@1041.com>
2022-06-10 23:40:21 +0800

    support more display

--- main.c
+++ main.c
@@ -7,7 +7,37 @@
 #include <string.h>
 
 #define MB ((size_t)(1<<20))
-#define DISK ((size_t)100 * MB)            // 100MB
+#define KB ((size_t)(1<<10))
+
+#define DISK          ((size_t)100 * MB)            // 100MB
+#define SECTOR_SIZE   ((size_t)512)                 // 512B
+#define CLUSTER_SIZE  ((size_t)8 * SECTOR_SIZE)     // 4KB
+#define FAT_COUNT     (DISK / CLUSTER_SIZE)         // 25600
+#define FAT_SIZE      ((size_t)4 * FAT_COUNT)       // 100KB
+#define DBR_SIZE      ((size_t)sizeof(struct DBR))
+
+struct DBR
+{
+    char flag[32];
+    int sector_size;
+    int cluster_size;
+    int fat_count;
+    int fat_size;
+};
+
+struct FAT
+{
+    uint cluster[FAT_COUNT];
+};
+
+struct FCB // SIZE == 64B
+{
+    char filename[32];
+    char time[16];   // yyyymmdd hhmmss
+    size_t size;     // file size, set to 0 for directory
+    int type;        // not used(0), folder(1), file(2), volume(3)
+    uint cluster;    // first cluster number of this file
+};
 
 
 int get_shm(void** pshm_buf, int* pshm_id)
@@ -40,6 +70,12 @@ void rm_shm(void* shm_buf, int shm_id)
     shmctl(shm_id, IPC_RMID, NULL);
 }
 
+void* cluster_to_pointer(void* buffer, int cluster)
+{
+    void* data = buffer + DBR_SIZE + FAT_SIZE + FAT_SIZE;
+    return data + (cluster - 2) * CLUSTER_SIZE;
+}
+
 
 int main(void)
 {
@@ -49,17 +85,49 @@ int main(void)
     if (get_shm(&shm_buf, &shm_id) == -1) return 0;
 
     // initialize disk ------------------------------
-    void* disk_buffer = shm_buf;
+    void* buffer = shm_buf;
 
     // get disk data
     char cmd[256];
+    char param[256];
 
+    printf(
+            "usage: \n"
+            "  exit\n"
+            "  fat\n"
+            "  fat+ [start]\n"
+            "  cl [cluster]\n"
+            "  [offset in bytes]\n"
+            );
+
+    size_t offset = 0;  // memory display offset
     while (1) {
         scanf("%s", cmd);
         if (0 == strcmp(cmd, "exit"))
             break;
-
-        size_t offset = strtol(cmd, NULL, 0);
+        else if (0 == strcmp(cmd, "fat")) {
+            uint* fat1 = ((struct FAT*)(buffer + DBR_SIZE))->cluster;
+            for (int i = 0; i < 20; i++) {
+                printf("%04x %08x\n", i, fat1[i]);
+            }
+            continue;
+        }
+        else if (0 == strcmp(cmd, "fat+")) {
+            uint* fat1 = ((struct FAT*)(buffer + DBR_SIZE))->cluster;
+            scanf("%s", param);
+            uint start = strtol(param, NULL, 0);
+            for (int i = start; i < start+20; i++) {
+                printf("%04x %08x\n", i, fat1[i]);
+            }
+            continue;
+        }
+        else if (0 == strcmp(cmd, "cl")) {
+            scanf("%s", param);
+            uint cluster = strtol(param, NULL, 0);
+            offset = cluster_to_pointer(buffer, cluster) - buffer;
+        }
+        else
+            offset = strtol(cmd, NULL, 0);
         for (int i = 0; i < 20; i++) {
             printf("0x%016x  ", (unsigned int)(offset + i*16));
             for (int j = 0; j < 16; j++) {
